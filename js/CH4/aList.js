f839f965ac071ce6b357d727208fbbddd7064cb7
Karsyz <mattkars@gmail.com>
2022-06-03 14:41:32 -0400

    updated aList.js

--- js/CH4/aList.js
+++ js/CH4/aList.js
@@ -15,55 +15,66 @@
 // The resulting objects form a chain, like this:
 
 // A linked list
+
 // A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, link: list} and {value: -1, link: list} (with list referring to the binding defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
 
-// Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+// Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+
+// Also write a listToArray function that produces an array from a list.
+
+// Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
 
 // If you haven’t already, also write a recursive version of nth.
 
 // Your code here.
+let list = {}
+let newArr = []
 
+function arrayToList(arr) {
+  let list = {}
 
-// let list = {value: 10, link: {value: 20, link: null} }
-// console.log(list)
-let arr = [10, 20]
-
-let num = {
-  value: null,
-  link: null
+  for (i = arr.length - 1; i >= 0; i--) {
+    list = {value: arr[i], link: list}
+  }
+  return list;
 }
 
-console.log(arr.length)
 
-for (i = 0; i < arr.length; i++) {
-  num.value = arr[i]
-  num.link = {num.value = arr
-}
-
-console.log(num)
-
-// function arrayToList(arr) {
-
-  
-//   var node = list;
-//   while(node) {
-//     array.push(node.value);
-//     node = node.link;
-//   }
-
-
-
-//   return node
-// }
-
-// console.log(arrayToList([10, 20]));
+console.log(arrayToList([10, 20, 30]));
 // → {value: 10, link: {value: 20, link: null}}
 
-// console.log(listToArray(arrayToList([10, 20, 30])));
+
+
+function listToArray(arr) {
+  let newArr = []
+  for (node = arr; node; node = node.link) {
+    newArr.push(node.value)
+  }
+  return newArr
+}
+
+console.log(listToArray( {value: 10, link: {value: 20, link: {value: 30, link: null} } } ) )
+
+console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 
-// console.log(prepend(10, prepend(20, null)));
-// // → {value: 10, link: {value: 20, link: null}}
+function prepend (elem, list){
+  var newList = {value: elem, link: list};
+  return newList;
+}
 
-// console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, link: {value: 20, link: null}}
+
+
+function nth (list, num){
+  if (!list){
+  	return undefined;
+  }else if (num === 0){
+  	return list.value;
+  }else {
+  	return nth(list.rest, num-1);
+  }
+}
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // // → 20
